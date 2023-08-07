cd9a3f4b5fb1e30f8b1fced3dfa286b6f2c3d700
chenzihao <1046683895@qq.com>
2023-08-07 10:42:46 +0800

    p74商品服务api品牌管理分类关联与级联更新
    实现了前端的增加和修改attr分组的功能，

--- renren-fast-vue/src/components/upload/multiUpload.vue
+++ renren-fast-vue/src/components/upload/multiUpload.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <el-upload
-      action="http://gulimall.oss-cn-shanghai.aliyuncs.com"
+      action="http://gulimall-czhczh.oss-cn-hangzhou.aliyuncs.com"
       :data="dataObj"
       list-type="picture-card"
       :file-list="fileList"
